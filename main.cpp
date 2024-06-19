9b5b66f1957638bb09c98b5d6a4e0ec68e3dea01
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-06-18 23:18:36 -0700

    Obtained adapter from webGPU and wrote functions to inspect its capabilities and properties.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,95 @@
 // Includes
 #include <webgpu/webgpu.h>
 #include <iostream>
+#include <cassert>
+#include <vector>
+
+// WebGPU adapter utility function
+WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions const * options) {
+
+    struct UserData {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded = false;
+    };
+    UserData userData;
+
+    // wgpuInstanceRequestAdapter callback
+    auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const * message, void * pUserData) {
+        UserData& userData = *reinterpret_cast<UserData*>(pUserData);
+        if (status == WGPURequestAdapterStatus_Success) {
+            userData.adapter = adapter;
+        } else {
+            std::cout << "Could not get WebGPU adapter: " << message << std::endl;
+        }
+        userData.requestEnded = true;
+    };
+
+    // Call to the WebGPU request adapter procedure
+    wgpuInstanceRequestAdapter(
+        instance,
+        options,
+        onAdapterRequestEnded,
+        (void*)&userData
+    );
+
+    assert(userData.requestEnded);
+
+    return userData.adapter;
+}
+
+void inspectAdapter(WGPUAdapter adapter) {
+#ifndef __EMSCRIPTEN__
+	WGPUSupportedLimits supportedLimits = {};
+	supportedLimits.nextInChain = nullptr;
+	bool success = wgpuAdapterGetLimits(adapter, &supportedLimits);
+	if (success) {
+		std::cout << "Adapter limits:" << std::endl;
+		std::cout << " - maxTextureDimension1D: " << supportedLimits.limits.maxTextureDimension1D << std::endl;
+		std::cout << " - maxTextureDimension2D: " << supportedLimits.limits.maxTextureDimension2D << std::endl;
+		std::cout << " - maxTextureDimension3D: " << supportedLimits.limits.maxTextureDimension3D << std::endl;
+		std::cout << " - maxTextureArrayLayers: " << supportedLimits.limits.maxTextureArrayLayers << std::endl;
+	}
+#endif // If not Emscripten
+	std::vector<WGPUFeatureName> features;
+
+	// First function call for entry count
+	size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+	// Allocate memory
+	features.resize(featureCount);
+
+	// Second function call with a non-null return address
+	wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+	std::cout << "Adapter features:" << std::endl;
+	std::cout << std::hex;
+	for (auto f : features) {
+		std::cout << " - 0x" << f << std::endl;
+	}
+	std::cout << std::dec;
+	WGPUAdapterProperties properties = {};
+	properties.nextInChain = nullptr;
+	wgpuAdapterGetProperties(adapter, &properties);
+	std::cout << "Adapter properties:" << std::endl;
+	std::cout << " - vendorID: " << properties.vendorID << std::endl;
+	if (properties.vendorName) {
+		std::cout << " - vendorName: " << properties.vendorName << std::endl;
+	}
+	if (properties.architecture) {
+		std::cout << " - architecture: " << properties.architecture << std::endl;
+	}
+	std::cout << " - deviceID: " << properties.deviceID << std::endl;
+	if (properties.name) {
+		std::cout << " - name: " << properties.name << std::endl;
+	}
+	if (properties.driverDescription) {
+		std::cout << " - driverDescription: " << properties.driverDescription << std::endl;
+	}
+	std::cout << std::hex;
+	std::cout << " - adapterType: 0x" << properties.adapterType << std::endl;
+	std::cout << " - backendType: 0x" << properties.backendType << std::endl;
+	std::cout << std::dec;
+}
 
 int main (int, char**) {
 
@@ -20,6 +109,20 @@ int main (int, char**) {
     // Display object (WGPUInstance is a simple pointer)
     std::cout << "WGPU instance: " << instance << std::endl;
 
-    // Destroy Instance
-    wgpuInstanceRelease(instance);
+	std::cout << "Requesting adapter..." << std::endl;
+	WGPURequestAdapterOptions adapterOpts = {};
+	adapterOpts.nextInChain = nullptr;
+	WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+	std::cout << "Got adapter: " << adapter << std::endl;
+
+	// Display some information about the adapter
+	inspectAdapter(adapter);
+
+	// We no longer need to use the instance once we have the adapter
+	wgpuInstanceRelease(instance);
+
+	wgpuAdapterRelease(adapter);
+
+	return 0;
+  
 }
